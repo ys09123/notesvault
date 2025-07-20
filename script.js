082136df6d6c287c1105c89eb44be57d9285b626
Harsh-26626 <harsh.ahuja.cs24@ggits.net>
2025-07-21 03:48:28 +0530

    created search form

---
+++ script.js
@@ -0,0 +1,135 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const search_branch = document.getElementById("search-parameters-branch");
+    const search_semester = document.getElementById("search-parameters-semester");
+    const search_subject = document.getElementById("search-parameters-subject");
+
+    if (search_branch) {
+        console.log("selected search form");
+    }
+
+    Branches = [];
+
+    fetch("data/search_parameters/parameters.json")
+        .then(res => res.json())
+        .then(data => {
+            const branchList = data.branches;
+            branchList.forEach(branch => {
+                if(branch.name){
+                    console.log(branch.name);
+                    Branches.push(branch.name);
+                }
+            });
+
+            console.log(Branches);
+
+            const chooseBranch = document.createElement("select");
+            chooseBranch.name = "selectBranch";
+            chooseBranch.id = "selectBranch";
+            chooseBranch.className = "search-parameters-select";
+            search_branch.appendChild(chooseBranch);
+            const chooseBranchDefault = document.createElement("option");
+            chooseBranchDefault.disabled = true;
+            chooseBranchDefault.selected = true;
+            chooseBranchDefault.innerHTML = "Select Branch";
+            chooseBranchDefault.className = "search-parameters-option";
+            const searchBranch = document.getElementById("selectBranch");
+            searchBranch.appendChild(chooseBranchDefault);
+
+            Branches.forEach(branch => {
+                const branchOption = document.createElement("option");
+                branchOption.value = branch;
+                branchOption.innerHTML = branch;
+                branchOption.className = "search-parameters-option";
+                searchBranch.appendChild(branchOption);
+            });
+            
+
+            const branchOptions = document.getElementById("selectBranch");
+            Semesters = [];
+            semesterList = [];
+
+            branchOptions.addEventListener("change", () => {
+                branchList.forEach(branch => {
+                    if(branch.name == branchOptions.value) {
+                        if(branch.semesters) {
+                            semesterList = branch.semesters;
+                            Semesters = [];
+                            semesterList.forEach(semester =>{
+                                Semesters.push(semester.semester);
+                            });
+                            console.log(Semesters);
+                        }
+                    }
+                });
+
+                search_semester.innerHTML = '';
+
+                const chooseSemester = document.createElement("select");
+                chooseSemester.name = "selectSemester";
+                chooseSemester.id = "selectSemester";
+                chooseSemester.className = "search-parameters-select";
+                search_semester.appendChild(chooseSemester);
+                const chooseSemesterDefault = document.createElement("option");
+                chooseSemesterDefault.disabled = true;
+                chooseSemesterDefault.selected = true;
+                chooseSemesterDefault.innerHTML = "Select Semester";
+                chooseSemesterDefault.className = "search-parameters-option";
+                const searchSemester = document.getElementById("selectSemester");
+                searchSemester.appendChild(chooseSemesterDefault);
+
+
+                semesterList.forEach(semester => {
+                    const semesterOption = document.createElement("option");
+                    semesterOption.value = semester.semester;
+                    semesterOption.innerHTML = semester.semester;
+                    semesterOption.className = "search-parameters-option";
+                    searchSemester.appendChild(semesterOption);
+                });
+
+                searchSemester.addEventListener("change", addSubjects());
+            });
+            Subjects = [];
+            subjectList = [];
+
+            function addSubjects() {
+                search_subject.innerHTML = "";
+                const selectSemester = document.getElementById("selectSemester");
+                selectSemester.addEventListener("change", () => {
+                    semesterList.forEach(semester => {
+                        if(semester){
+                            if(semester.semester == selectSemester.value){
+                                subjectList = semester.subjects;
+                                Subjects = [];
+                                subjectList.forEach(subject => {
+                                    Subjects.push(Object.values(subject));
+                                });
+                            }
+                        }
+                    });
+                    const chooseSubject = document.createElement("select");
+                    chooseSubject.name = "selectSubject";
+                    chooseSubject.id = "selectSubject";
+                    chooseSubject.className = "search-parameters-select";
+                    search_subject.appendChild(chooseSubject);
+                    const chooseSubjectDefault = document.createElement("option");
+                    chooseSubjectDefault.disabled = true;
+                    chooseSubjectDefault.selected = true;
+                    chooseSubjectDefault.innerHTML = "Select Subject";
+                    chooseSubjectDefault.className = "search-parameters-option";
+                    const searchSubject = document.getElementById("selectSubject");
+                    searchSubject.appendChild(chooseSubjectDefault);
+
+                    Subjects.forEach(subject => {
+                        console.log(subject);
+                        const subjectOption = document.createElement("option");
+                        subjectOption.value = subject;
+                        subjectOption.innerHTML = subject;
+                        subjectOption.className = "search-parameters-option";
+                        searchSubject.appendChild(subjectOption);
+                    });
+                });
+            }
+            
+        });
+})
+
